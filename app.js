b6a894eceba6996550eb5d1eeb6d96ffcf7d80b9
Tatah Withney <164454406+tatahwithney@users.noreply.github.com>
2026-08-19 13:07:37 +0100

    Add EJS partials and active navigation and also passed active page from routing

--- app.js
+++ app.js
@@ -12,33 +12,56 @@ app.set("view engine", "ejs");
 //rout to home page and return server date and time
 app.get("/", (req, res) => {
     res.render("home", {
-        currentTime: new Date().toLocaleString()
+        currentTime: new Date().toLocaleString(),
+        activePage: "home"
     });
 });
+
 //route to the about page on /about request
 app.get("/about", (req, res) => {
-    res.render("about");
+    res.render("about", {
+        activePage: "about"
+    });
 });
 
+//render the form page
+app.get("/form", (req, res) => {
+    res.render("form", {
+        activePage: "form"
+    });
+})
+
+//render the index page, we donot explicitly refer to this as our home page
+app.get("/index", (req, res) =>{
+    res.render("index", {
+        activePage: "index"
+    });
+})
+const maxAge = 25;
+const minAge = 18;
 //Display key:value pairs for users in the object users
 app.get("/users", (req, res) => {
     const users = [
         {
             name: "Tatah Withney",
-            email: "tatah@example.com"
+            email: "tatah@example.com",
+            age: Math.floor(Math.random()*(maxAge-minAge+1)) + minAge
         },
         {
             name: "John Doe",
-            email: "john@example.com"
+            email: "john@example.com",
+            age: Math.floor(Math.random()*(maxAge-minAge+1)) + minAge
         },
         {
             name: "Jane Doe",
-            email: "jane@example.com"
+            email: "jane@example.com",
+            age: Math.floor(Math.random()*(maxAge-minAge+1)) + minAge
         }
     ];
 
     res.render("users", {
-        users: users
+        users: users,
+        activePage: "users"
     });
 });
 
@@ -46,3 +69,5 @@ app.get("/users", (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
+message = "Hey do you see me?"
